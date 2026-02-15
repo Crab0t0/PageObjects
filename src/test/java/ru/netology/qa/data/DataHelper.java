5ba4ead321da46f7b2b2c492846363e81d2009c8
Gomaz A. <pogis@inbox.ru>
2026-02-15 21:36:53 +0300

    update config

--- src/test/java/ru/netology/qa/data/DataHelper.java
+++ src/test/java/ru/netology/qa/data/DataHelper.java
@@ -22,6 +22,14 @@ public class DataHelper {
     public static CardInfo getSecondCardInfo() {
         return new CardInfo("0f3f5c2a-249e-4c3d-8287-09f7a039391d", "5559 0000 0000 0002");
     }
+    public static int generateValidAmount(int balance) {
+        if (balance <= 0) return 0;
+        var random = new java.util.Random();
+        return random.nextInt(balance) + 1;
+    }
+    public static int generateInvalidAmount(int balance) {
+        return balance + 100;
+    }
 
     @Value
     public static class AuthInfo {
@@ -37,6 +45,6 @@ public class DataHelper {
     @Value
     public static class CardInfo {
         String id;
-        String Number;
+        String number;
     }
 }
--- src/test/java/ru/netology/qa/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/qa/test/MoneyTransferTest.java
@@ -6,6 +6,7 @@ import ru.netology.qa.page.LoginPage;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MoneyTransferTest {
     @Test
@@ -20,10 +21,33 @@ class MoneyTransferTest {
         int startBalanceFirst = dashboardPage.getCardBalance(firstCardInfo);
         int startBalanceSecond = dashboardPage.getCardBalance(secondCardInfo);
         var transferPage = dashboardPage.selectCard(firstCardInfo);
-        int amount = 500;
+        int amount = DataHelper.generateValidAmount(startBalanceSecond);
         dashboardPage = transferPage.makeTransfer(String.valueOf(amount), secondCardInfo);
         assertEquals(startBalanceFirst + amount, dashboardPage.getCardBalance(firstCardInfo));
         assertEquals(startBalanceSecond - amount, dashboardPage.getCardBalance(secondCardInfo));
+        assertTrue(dashboardPage.getCardBalance(secondCardInfo) >= 0,
+                "Баланс карты списания ушел в минус: " + dashboardPage.getCardBalance(secondCardInfo));
+    }
+
+    @Test
+    void shouldNotTransferAmountMoreThanBalance() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCode();
+        var dashboardPage = verificationPage.validVerify(verificationCode);
+
+        DataHelper.CardInfo firstCardInfo = DataHelper.getFirstCardInfo();
+        var secondCardInfo = DataHelper.getSecondCardInfo();
+        int startBalanceSecond = dashboardPage.getCardBalance(secondCardInfo);
+
+        var transferPage = dashboardPage.selectCard(firstCardInfo);
+        int amount = DataHelper.generateInvalidAmount(startBalanceSecond);
+        dashboardPage = transferPage.makeTransfer(String.valueOf(amount), secondCardInfo);
+
+        int actualBalanceSecond = dashboardPage.getCardBalance(secondCardInfo);
+
+        assertEquals(startBalanceSecond, actualBalanceSecond, "Баланс карты изменился!");
     }
 
 
